48a57961feb9a06f9b5f17628bf39cd4e1c4df3d
student <student@C251U44>
2022-09-07 11:30:27 +0300

    Добавьте файлы проекта.

---
+++ first project/first project.cpp
@@ -0,0 +1,15 @@
+﻿// first project.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 30; i++)
+		Beep(400 + i * 50, 1000 - i * 40);
+}
+
+
